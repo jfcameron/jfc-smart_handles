e7af1a48a995c154287297223b92631890d3deac
Joseph Cameron <jf.cameron@gmail.com>
2019-11-03 18:16:35 -0500

    added test for shared_handle, added use_count method

--- include/jfc/shared_handle.h
+++ include/jfc/shared_handle.h
@@ -13,7 +13,6 @@ namespace jfc
     /// Deleter will only be called once: when final the copy of the smart handle goes out of scope.
     /// Implementation based on final_action found in the Cpp General Guidelines, extended to strongly associate the handle with the deletor (both are members) and to support copy operations in addition to moves
     /// analogous to stl shared_ptr
-    /// This is a "Thread friendly" object. const methods are safe. Care must be taken around the end of life, however. For example, if an instance is destructing such that use_count returns 1 just before some other thread increments the count, the deleter will be invoked, which will turn the new instance on the other thread a zombie. This is because smart_ptr<>::use_count is not explicitly synchronized. This can be detected (count will then return 0). I believe this is an acceptable edgecase and would rather warn about end of life issue rather than introduce a mutex.
     template<class handle_type_param>
     class shared_handle final
     {
@@ -22,17 +21,66 @@ namespace jfc
         using deleter_type = std::function<void(const handle_type)>;
 
     private: 
+        /// \brief handle to the resource
         handle_type m_Handle;
 
+        /// \brief a pointer to the deletor shared across moved/copied instances. Called when the final co-owning instance of the handle falls out of scope, cleaning up the resource.
         std::shared_ptr<deleter_type> m_pDeleter;
 
     public:
-        //TODO: enable/disable ref depending on handle_type? if sizeof(handle_type) < sizeof(handle_type *), no ref.
-        const handle_type &get() const
+        /// \brief gets a copy of the handle's value
+        handle_type get() const
         {
             return m_Handle;
         }
 
+        /// \brief returns number of coowners of this handle
+        long use_count() const
+        {
+            return m_pDeleter.use_count();
+        }
+        
+        /// \brief equality operators
+        bool operator==(const shared_handle<handle_type> &b) const
+        {
+            return 
+                m_Handle   == b.m_Handle && 
+                m_pDeleter == b.m_pDeleter;
+        }
+        
+        /// \brief equality operators
+        bool operator!=(const shared_handle<handle_type> &b) const
+        {
+            return 
+                m_Handle   != b.m_Handle || 
+                m_pDeleter != b.m_pDeleter;
+        }
+
+        /// \brief copy semantics
+        shared_handle(const shared_handle<handle_type> &b)
+        : m_Handle(b.get())
+        , m_pDeleter(b.m_pDeleter)
+        {}
+        /// \brief copy semantics
+        shared_handle &operator=(const shared_handle<handle_type> &b) const
+        {
+            return shared_handle(b);
+        }
+
+        /// \brief move semantics
+        shared_handle(shared_handle<handle_type> &&b)
+        : m_Handle(std::move(b.m_Handle))
+        , m_pDeleter(std::move(b.m_pDeleter))
+        {}
+        /// \brief move semantics
+        shared_handle &operator=(shared_handle<handle_type> &&b) const
+        {
+            return std::move(b);
+        }
+
+        /// \brief takes a handle and a functor which contains cleanup logic for the handle.
+        /// cleanup is delayed to when the final instance of shared_handle which owns the same resource
+        /// falls out of scope.
         shared_handle(handle_type &&aValue, deleter_type &&aDeleter)
         : m_Handle(aValue)
         , m_pDeleter(std::make_shared<shared_handle<handle_type>::deleter_type>(aDeleter))
--- test/shared_handle_test.cpp
+++ test/shared_handle_test.cpp
@@ -5,12 +5,72 @@
 
 #include <jfc/shared_handle.h>
 
+#include <cstddef>
+
+using namespace jfc;
+
+using glint = int;
+using glfloat = float;
+
+static constexpr glint INITIALIZED(1);
+
+glint glGenBuffer()
+{
+    return INITIALIZED;
+}
+
 TEST_CASE( "jfc::shared_handle test", "[jfc::shared_handle]" )
 {
-    SECTION("")
+    bool deleterCalled(false);
+
+    auto glDeleteBuffer = [&](glint)
     {
-        //
-        REQUIRE(true);
+        deleterCalled = true;    
+    };
+
+    shared_handle<glint> shared(glGenBuffer(), [&](glint a)
+    {
+        glDeleteBuffer(a);
+    });
+    
+    REQUIRE(!deleterCalled);
+
+    SECTION("constructed instance contains a well formed handle")
+    {
+        REQUIRE(shared.get() == INITIALIZED);
+    }
+
+    SECTION("Copy semantics work as expected, equality semantics too")
+    {
+        const auto two = shared;
+        {
+            REQUIRE(shared == two);
+
+            const decltype(two) three(two);
+
+            REQUIRE(two == three);
+
+            const decltype(two) four(glGenBuffer(), [&](glint){});
+
+            REQUIRE(four != three);
+
+            REQUIRE(two.use_count() == 3);
+        }
+
+        REQUIRE(!deleterCalled);
+    }
+
+    SECTION("move semantics")
+    {
+        REQUIRE(!deleterCalled);
+
+        {
+            auto moved = std::move(shared);
+
+            REQUIRE(moved.get() == INITIALIZED);
+        }
+
+        REQUIRE(deleterCalled);
     }
 }
 
