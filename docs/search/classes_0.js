c89b234c422f40230bd5aa363e3304a2cd1e468d
Travis CI User <travis@example.org>
2019-11-17 04:23:38 +0000

    docs update

---
+++ docs/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['shared_5fhandle',['shared_handle',['../classjfc_1_1shared__handle.html',1,'jfc']]]
+];
---
+++ docs/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lock',['lock',['../classjfc_1_1weak__handle.html#abf0db42ed3caa4c050e7de887b672039',1,'jfc::weak_handle']]]
+];
